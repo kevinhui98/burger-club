eac7000bdca956adbdd74cd3b11388be8e5eedb8
kevinhui98 <kevin.hui@envirostudies.org>
2017-06-29 19:50:08 +0000

    completing everything on the check list

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-
-   
+    $('#myModal').on('show.bs.modal', function (event) {
     
-
+        var modal = $(this)
+        modal.find('#content').html("Hello " + $('#name').val() + " thank you for signing up! We will send an email to " + $('#email').val() + " shortly");
+    });
+    
 });
